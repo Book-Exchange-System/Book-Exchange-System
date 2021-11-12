547d91d6bca423354ae99fa9fa69e82c5fee3acb
Osman MUTLU <osman_mutlu_2205@hotmail.com>
2021-11-12 10:34:05 +0300

    Upgrade project

--- Project Solition/BookStoreProject/BookStoreProject/Controllers/HomeController.cs
+++ Project Solition/BookStoreProject/BookStoreProject/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace BookStoreProject.Controllers
             return View();
         }
 
+
         public IActionResult Privacy()
         {
             return View();
--- Project Solition/BookStoreProject/BookStoreProject/wwwroot/js/site.js
+++ Project Solition/BookStoreProject/BookStoreProject/wwwroot/js/site.js
@@ -2,7 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 $(".wrapper-menu").click(function () {
     $("#sidebar").toggleClass("clicked");
-    $("#content-page").toggleClass("clicked");
     return false;
 });
 // Write your JavaScript code.
