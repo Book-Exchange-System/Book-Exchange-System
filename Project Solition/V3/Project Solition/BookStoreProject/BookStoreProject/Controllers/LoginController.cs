c8f2ee19d88abcc43a8e160c284bc228e5380ce4
ecem <ecemsevvalcinar@gmail.com>
2021-12-19 18:28:52 +0300

    V3

---
+++ Project Solition/V3/Project Solition/BookStoreProject/BookStoreProject/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using BookStoreProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Collections.Generic;
+
+namespace BookStoreProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        UserController userController = new UserController();
+        [Route("login")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            if (userController.CheckUser(user))
+            {
+                ViewBag.user = user;
+                ViewBag.isLogin = true;
+                return View("index");
+            }
+            else
+            {
+                String errorMessage = "";
+                ViewBag.errorMessage = errorMessage;
+                return View();
+            }
+            
+        }
+        [HttpGet] 
+        public IActionResult Register() // bombos
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user) // formu doldurup submit ederse
+        {
+            if (ModelState.IsValid)
+            {
+                //database kayıt işlemleri
+                if (!userController.HasUser(user))
+                {
+                    userController.AddUser(user);
+                    return View("login");
+                }
+                else
+                {
+                    String errorMessage = "This username is already taken!";
+                    ViewBag.errorMessage = errorMessage;
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+            
+            
+        }
+    }
+}
---
+++ Project Solition/V3/Project Solition/BookStoreProject/BookStoreProject/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreProject.Models
+{
+    public class User
+    {
+        public Int64 ID { get; set; }
+
+        [Required(ErrorMessage="Enter Your username.")]
+        public string Username { get; set; }
+
+
+        [Required]
+        [StringLength(20,ErrorMessage ="Your password must be at least 6 characters long!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Surname.")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Choose Your Birth Date.")]
+        public DateTime Birthday { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Phone Number.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Email.")]
+        [EmailAddress(ErrorMessage ="Enter Your Email in Correct Form!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Choose Your Gender!")]
+        [StringLength(1, ErrorMessage = "Choose Your Gender!")]
+        public string Gender { get; set; }
+        public string[] WantsToReadList { get; set; }
+        public string[] ReadAlreadyList { get; set; }
+        public string[] NowReading { get; set; }
+        public string[] BooksOwned { get; set; }
+    }
+}
