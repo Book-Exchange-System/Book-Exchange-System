c1307b99a09f90e732275c8ba109a7ba990463ca
ecem <ecemsevvalcinar@gmail.com>
2022-01-01 22:31:08 +0300

    V9

---
+++ Project Solition/v9/BookStoreProject/BookStoreProject/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using BookStoreProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Collections.Generic;
+
+namespace BookStoreProject.Controllers
+{
+    [Obsolete]
+    public class HomeController : Controller
+    {
+        BookController bookController = new BookController();
+        
+        
+        public IActionResult Index()
+        {
+            String comd = "SELECT TOP 12 * FROM[bookexchange].[dbo].[Book]";
+            List<Book> books = bookController.FetchBook(comd);
+            ViewBag.top12books = books;
+            comd = "SELECT * FROM [dbo].[Book] where Book_ID=(select max(Book_ID)from Book)";
+            books = bookController.FetchBook(comd);
+            ViewBag.FeaturedBook = books[0];
+            comd = "SELECT Top 10 * FROM [dbo].[Book] Order By Book_Rating Desc";
+            books = bookController.FetchBook(comd);
+            ViewBag.FavoriteBooks = books;
+            return View();
+        }
+        
+        
+        
+       
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Project Solition/v9/BookStoreProject/BookStoreProject/Controllers/PurchaseController.cs
@@ -0,0 +1,81 @@
+﻿using BookStoreProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BookStoreProject.Controllers
+{
+
+
+    public class PurchaseController : Controller
+    {
+
+
+
+        BookController bookController = new BookController();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection(@"Data Source=desktop-mruiip9;Initial Catalog=bookexchange;Integrated Security=True");
+
+        List<User> users = new List<User>();
+
+        [HttpGet]
+        [Route("payment")]
+        public IActionResult Payment()
+        {
+            List<Book> shoppingCard = BookController.shoppingCard;
+            if (shoppingCard.Count != 0 && LoginController.currentUser!=null)
+            {
+
+                ViewBag.shoppingCard = shoppingCard;
+                    return View(); 
+            }
+            else if (LoginController.currentUser == null && shoppingCard.Count != 0)
+            {
+                return RedirectToAction("login", "login");
+            }
+            else
+            {
+                return RedirectToAction("index", "home");
+            }
+            
+        }
+
+        [HttpPost]
+        public IActionResult Payment(Purchase purchase)
+        {
+            String books = "";
+            int counter = 0;
+            foreach(Book book in BookController.shoppingCard)
+            {
+                if (counter == 0) books += book.ID;
+                else books += (";" + book.ID);
+                counter++;
+                purchase.BookID = book.ID;
+                purchase.Price = book.Price;
+                purchase.Date = DateTime.Now;
+                purchase.UserID = LoginController.currentUser.ID;
+                string cmd = "INSERT into [dbo].[Purchase](BookID,UserID,Date,Price,CardNumber,CVC,NameOnCard,ExpireDate) values('"+purchase.BookID+"','"+ purchase.UserID + "','" + purchase.Date.ToString("yyyy-MM-dd") + "','" + purchase.Price + "','" + purchase.CardNumber + "','" + purchase.CVC + "','" + purchase.NameOnCard + "','" + (purchase.ExpireDateMonth + "/" + purchase.ExpireDateYear) + "');";
+                con.Open();
+                com = con.CreateCommand();
+                com.CommandType = CommandType.Text;
+                com.CommandText = cmd;
+                com.ExecuteNonQuery();
+                con.Close();
+            }
+            if (LoginController.currentUser.BooksOwned.Length > 0)
+            {
+                LoginController.currentUser.BooksOwned += (";" + books);
+            }
+            else
+            {
+                LoginController.currentUser.BooksOwned = books;
+            }
+            UserController.UpdateCurrentUser();
+            BookController.shoppingCard = new List<Book>();
+            return RedirectToAction("index", "home");
+        }
+    }
+}
